d6fd3c8f846ecd5403aa00e43ccb5fe885f2ec85
Team Tyaa <team.tyaa.pastime@gmail.com>
2026-03-22 22:38:23 +0900

    初期構成

---
+++ README.md
@@ -0,0 +1,3 @@
+# not_enough_sake
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/not_enough_sake/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.not_enough_sake
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
